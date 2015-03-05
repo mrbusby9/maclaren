cf90c472912d9e7ce25cd260eac5b1133eb6e2e2
Matthew Busby <mbusby@arn.com>
2015-03-04 21:45:07 -0500

    updatesssss

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,4 +5,14 @@ $(document).ready(function () {
       effect : "fadeIn"
   });
   
+  $(".lazy").lazyload({
+    threshold : 200
+  });
+  
+});
+
+$(window).bind("load", function() {
+  var timeout = setTimeout(function() {
+      $(".lazy").trigger("sporty")
+  }, 3000);
 });
