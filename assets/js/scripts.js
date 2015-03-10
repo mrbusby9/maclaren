e94dd9802c9aea54775accbf17bd55ff6dfc9030
Matthew Busby <mbusby@arn.com>
2015-03-10 17:16:13 -0400

    updates

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
   });
   
   $('.scrollToTop').click(function(){
-    $('html, body').animate({scrollTop : 0},800);
+    $('html, body').animate({scrollTop : 0},1600);
     return false;
   });  
   
